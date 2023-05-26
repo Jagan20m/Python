2358b290fab4271133ddbffc43c14437523dab48
Jagan20m <115446888+Jagan20m@users.noreply.github.com>
2023-05-26 20:49:43 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def deez(name):
+    print("yo",name)
+pes=input("enter the name : ")
+deez(pes)
+
+
+# In[2]:
+
+
+def add(num1,num2):
+    print("enter the first number : ",num1)
+    print("enter the second the number : ",num2)
+    
+num1=
+
+
+# In[4]:
+
+
+def addition(num1,num2):
+    add=num1+num2
+    return(add)
+num1=int(input("Enter the first number :"))
+num2=int(input("enter the second number :"))
+addition(num1,num2)
+
+
+# In[6]:
+
+
+def add(num1,num2):
+    print("first argument is ",num1)
+    print("second argument is :",num2)
+num1=6
+num2=4
+add(num1,num2)
+
+
+# In[8]:
+
+
+def hell(name):
+    print("yo "+name+" hi")
+hell("jake")
+
+
+# In[9]:
+
+
+def employee(job,designation):
+    print(job,designation)
+employee("jake","ceo")
+
+
+# In[10]:
+
+
+def emp(name,sal=12000):
+    print("empname,sal)
+
+
+# In[ ]:
+
+
+
+
